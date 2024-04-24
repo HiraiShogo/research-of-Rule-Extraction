351d3fe27d1f59b5400050bb457a03ed2a2bf965
som <daisannsuu@yahoo.co.jp>
2024-04-25 02:29:40 +0900

    Initial commit

---
+++ CNN_MNIST/test.py
@@ -0,0 +1,19 @@
+import keras
+from keras.datasets import mnist
+import numpy as np
+from PIL import Image
+
+# 文字画像表示
+def img_show(img):
+    pil_img = Image.fromarray(np.uint8(img))
+    pil_img.show()
+
+# Kerasの関数でデータの読み込み。データをシャッフルして学習データと訓練データに分割
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# MNISTの文字画像を読み出して表示
+num = 1
+for i in range(num):
+    print(x_train[i].reshape(28, 28).shape)
+    print(type(x_train[i].reshape(28, 28)))
+    img_show(x_train[i].reshape(28, 28))
